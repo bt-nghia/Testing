37cd3ed5d2dcd52bc63c09a75b532d836878f048
bt-nghia <nghiak99lhp@gmail.com>
2023-09-24 14:19:55 +0700

    equi

--- blackbox1/equivalencePartition.py
+++ blackbox1/equivalencePartition.py
@@ -3,28 +3,20 @@ from busfee import *
 
 class EquiPartition(unittest.TestCase):
     def test1(self):
-        self.assertEqual(busTicketDiscount(1, False), (1, 50))
+        self.assertEqual(busTicketDiscount(1, True), (1, 10))
         return
     
     def test2(self):
-        self.assertEqual(busTicketDiscount(2, False), (2, 30))
+        self.assertEqual(busTicketDiscount(3, True), (3, 0))
         return
     
     def test3(self):
-        self.assertEqual(busTicketDiscount(3, False), (3, 10))
+        self.assertEqual(busTicketDiscount(1, False), (1, 50))
         return
 
     def test4(self):
-        self.assertEqual(busTicketDiscount(1, True), (1, 10))
+        self.assertEqual(busTicketDiscount(2, False), (2, 30))
         return
-
-    def test5(self):
-        self.assertEqual(busTicketDiscount(2, True), (2, 0))
-        return    
-    
-    def test6(self):
-        self.assertEqual(busTicketDiscount(3, True), (3, 0))
-        return  
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
